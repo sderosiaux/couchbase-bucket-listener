181a35c21d872f617d06664d6762ca6578d56c16
Stéphane Derosiaux <sderosiaux@powerspace.com>
2017-03-07 10:23:57 +0100

    Add custom bucket listener

--- src/main/scala/com/ctheu/couchbase/CouchbaseListener.scala
+++ src/main/scala/com/ctheu/couchbase/CouchbaseListener.scala
@@ -1,8 +1,6 @@
 package com.ctheu.couchbase
 
-import java.time.LocalTime
-import java.time.format.DateTimeFormatter
-import java.util.concurrent.{CompletableFuture, ConcurrentLinkedQueue, ConcurrentSkipListSet}
+import java.util.concurrent.ConcurrentLinkedQueue
 import java.util.concurrent.atomic.LongAdder
 
 import akka.NotUsed
@@ -11,41 +9,47 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Source
-import com.couchbase.client.dcp.conductor.{Conductor, ConfigProvider, HttpStreamingConfigProvider}
-import com.couchbase.client.dcp.config.{ClientEnvironment, DcpControl}
-import com.couchbase.client.dcp.message.{DcpDeletionMessage, DcpExpirationMessage, DcpMutationMessage, DcpSnapshotMarkerRequest}
-import com.couchbase.client.dcp.state.StateFormat
 import com.couchbase.client.dcp._
-import com.couchbase.client.deps.io.netty.buffer.ByteBuf
-import com.couchbase.client.deps.io.netty.channel.nio.NioEventLoopGroup
+import com.couchbase.client.dcp.config.DcpControl
+import com.couchbase.client.dcp.message.{DcpDeletionMessage, DcpExpirationMessage, DcpMutationMessage, DcpSnapshotMarkerRequest}
 import de.heikoseeberger.akkasse.ServerSentEvent
-import rx.Completable
-
 import collection.JavaConverters._
-import scala.collection.mutable
 
 object CouchbaseListener extends App {
 
-  def conductor() = {
-    val group = new NioEventLoopGroup
-    val env = ClientEnvironment.builder
-      .setClusterAt(List("couchbase01.stg.ps").asJava)
-      .setConnectionNameGenerator(DefaultConnectionNameGenerator.INSTANCE)
-      .setBucket("budget")
-      .setDcpControl(new DcpControl)
-      .setEventLoopGroup(group, true)
-      .build
+//  def conductor() = {
+//    val group = new NioEventLoopGroup
+//    val env = ClientEnvironment.builder
+//      .setClusterAt(List("couchbase01.stg.ps").asJava)
+//      .setConnectionNameGenerator(DefaultConnectionNameGenerator.INSTANCE)
+//      .setBucket("budget")
+//      .setDcpControl(new DcpControl)
+//      .setEventLoopGroup(group, true)
+//      .build
+//
+//    val ctor = new Conductor(env, new HttpStreamingConfigProvider(env))
+//    ctor.connect().await()
+//
+//    Thread.sleep(1000)
+//    ctor.stop().andThen(env.shutdown())
+//  }
 
-    val ctor = new Conductor(env, new HttpStreamingConfigProvider(env))
-    ctor.connect().await()
 
-    Thread.sleep(1000)
-    ctor.stop().andThen(env.shutdown())
-  }
+  implicit val system = ActorSystem("couchbase-listener")
+  implicit val materializer = ActorMaterializer()
+  implicit val executionContext = system.dispatcher
 
-  def run() = {
+
+  case class Stats(
+                    mutation: LongAdder = new LongAdder,
+                    deletion: LongAdder = new LongAdder,
+                    expiration: LongAdder = new LongAdder,
+                    lastDocsMutated: ConcurrentLinkedQueue[String] = new ConcurrentLinkedQueue[String]()
+                  )
+
+  def listenTo(bucket: String) = {
     val client = Client.configure()
-      .bucket("budget")
+      .bucket(bucket)
       .hostnames("couchbase01.stg.ps")
       .controlParam(DcpControl.Names.CONNECTION_BUFFER_SIZE, 10000) // set the buffer to 10K
       .bufferAckWatermark(75) // after 75% are reached of the 10KB, acknowledge against the serv
@@ -58,26 +62,23 @@ object CouchbaseListener extends App {
       event.release()
     }
 
-    val mutation = new LongAdder
-    val deletion = new LongAdder
-    val expiration = new LongAdder
-    var lastDocsMutated = new ConcurrentLinkedQueue[String]()
+    val stats = Stats()
 
     client.dataEventHandler { event =>
       if (DcpMutationMessage.is(event)) {
         println("mutation: " + DcpMutationMessage.toString(event))
-        mutation.increment()
-        lastDocsMutated.add(DcpMutationMessage.keyString(event))
-        if (lastDocsMutated.size > 10)
-          lastDocsMutated.poll()
+        stats.mutation.increment()
+        stats.lastDocsMutated.add(DcpMutationMessage.keyString(event))
+        if (stats.lastDocsMutated.size > 10)
+          stats.lastDocsMutated.poll()
       }
       else if (DcpDeletionMessage.is(event)) {
         println("deletion: " + DcpMutationMessage.toString(event))
-        deletion.increment()
+        stats.deletion.increment()
       }
       else if (DcpExpirationMessage.is(event)) {
         println("expiration: " + DcpMutationMessage.toString(event))
-        expiration.increment()
+        stats.expiration.increment()
       }
       else {
         println("unknown")
@@ -89,84 +90,81 @@ object CouchbaseListener extends App {
     client.initializeState(StreamFrom.NOW, StreamTo.INFINITY).await()
     client.startStreaming().await()
 
-
-    implicit val system = ActorSystem("couchbase-listener")
-    implicit val materializer = ActorMaterializer()
-    // needed for the future flatMap/onComplete in the end
-    implicit val executionContext = system.dispatcher
-
-    def route = {
-      import akka.http.scaladsl.server.Directives._
-      import de.heikoseeberger.akkasse.EventStreamMarshalling._
-      import concurrent.duration._
-
-      path("events") {
-        get {
-          complete {
-            Source.tick(1 second, 1 second, NotUsed)
-              .map(_ => ServerSentEvent(s"""
-                             | {
-                             |  "mutation": ${mutation.longValue()},
-                             |  "deletion": ${deletion.longValue()},
-                             |  "expiration": ${expiration.longValue()},
-                             |  "lastDocsMutated": [${lastDocsMutated.asScala.mkString("\"",",","\"")}]
-                             | }
-                              """.stripMargin))
-              .keepAlive(1.second, () => ServerSentEvent.heartbeat)
-          }
-        }
-      } ~ path("ui") {
-        get {
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`,
-            """
-              |<html>
-              |<head>
-              |</head>
-              |<body>
-              |<h3>Mutations</h3>
-              |<canvas id="mutation" width="400" height="100"></canvas>
-              |<h3>Deletions</h3>
-              |<canvas id="deletion" width="400" height="100"></canvas>
-              |<h3>Expirations</h3>
-              |<canvas id="expiration" width="400" height="100"></canvas>
-              |<h3>Last documentation mutated</h3>
-              |<pre id="lastMutation"></pre>
-              |</div>
-              |<script src="//cdnjs.cloudflare.com/ajax/libs/smoothie/1.27.0/smoothie.min.js"></script>
-              |<script>
-              |function ch(id) {
-              |const chart = new SmoothieChart({millisPerPixel:100,maxValueScale:1.5,grid:{strokeStyle:'rgba(119,119,119,0.43)'}});
-              |const series = new TimeSeries();
-              |chart.addTimeSeries(series, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 4 });
-              |chart.streamTo(document.getElementById(id), 1000);
-              |return series;
-              |}
-              |const mut = ch("mutation")
-              |const del = ch("deletion")
-              |const exp = ch("expiration")
-              |
-              |var source = new EventSource('events');
-              |source.addEventListener('message', function(e) {
-              |  var data = JSON.parse(e.data);
-              |  mut.append(new Date().getTime(), data.mutation);
-              |  del.append(new Date().getTime(), data.deletion);
-              |  exp.append(new Date().getTime(), data.expiration);
-              |  document.getElementById("lastMutation").innerHTML = data.lastDocsMutated;
-              |}, false);
-              |</script>
-              |</body>
-              |</html>
-              |
-            """.stripMargin
-          ))
-        }
-      }
-    }
-
-    Http().bindAndHandle(route, "localhost", 8080)
+    stats
   }
 
 
-  run()
+  def route = {
+    import akka.http.scaladsl.server.Directives._
+    import de.heikoseeberger.akkasse.EventStreamMarshalling._
+
+    import concurrent.duration._
+
+    path("events" / """[a-z_]+""".r) { bucket =>
+      get {
+        complete {
+          val stats = listenTo(bucket)
+
+          Source.tick(1 second, 1 second, NotUsed)
+            .map(_ => ServerSentEvent(s"""
+                           | {
+                           |  "mutation": ${stats.mutation.longValue()},
+                           |  "deletion": ${stats.deletion.longValue()},
+                           |  "expiration": ${stats.expiration.longValue()},
+                           |  "lastDocsMutated": [${stats.lastDocsMutated.asScala.mkString("\"",",","\"")}]
+                           | }
+                            """.stripMargin))
+            .keepAlive(1.second, () => ServerSentEvent.heartbeat)
+        }
+      }
+    } ~ path("ui" / """[a-z_]+""".r) { bucket =>
+      get {
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`,
+          s"""
+            |<html>
+            |<head>
+            |</head>
+            |<body>
+            |<h3>Mutations</h3>
+            |<canvas id="mutation" width="400" height="100"></canvas>
+            |<h3>Deletions</h3>
+            |<canvas id="deletion" width="400" height="100"></canvas>
+            |<h3>Expirations</h3>
+            |<canvas id="expiration" width="400" height="100"></canvas>
+            |<h3>Last documentation mutated</h3>
+            |<pre id="lastMutation"></pre>
+            |</div>
+            |<script src="//cdnjs.cloudflare.com/ajax/libs/smoothie/1.27.0/smoothie.min.js"></script>
+            |<script>
+            |function ch(id) {
+            |const chart = new SmoothieChart({millisPerPixel:100,maxValueScale:1.5,grid:{strokeStyle:'rgba(119,119,119,0.43)'}});
+            |const series = new TimeSeries();
+            |chart.addTimeSeries(series, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 4 });
+            |chart.streamTo(document.getElementById(id), 1000);
+            |return series;
+            |}
+            |const mut = ch("mutation")
+            |const del = ch("deletion")
+            |const exp = ch("expiration")
+            |
+            |var source = new EventSource('/events/$bucket');
+            |source.addEventListener('message', function(e) {
+            |  var data = JSON.parse(e.data);
+            |  mut.append(new Date().getTime(), data.mutation);
+            |  del.append(new Date().getTime(), data.deletion);
+            |  exp.append(new Date().getTime(), data.expiration);
+            |  document.getElementById("lastMutation").innerHTML = data.lastDocsMutated.map(x => x + "<br>");
+            |}, false);
+            |</script>
+            |</body>
+            |</html>
+            |
+          """.stripMargin
+        ))
+      }
+    }
+  }
+
+  Http().bindAndHandle(route, "0.0.0.0", 8080)
 
 }
